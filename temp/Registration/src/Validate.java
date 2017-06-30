85bfc87da932e61bc89ba13604260326ec18379e
GaborVeress <veressgabi33@freemail.hu>
2017-06-30 09:28:41 +0200

    reupload

--- temp/Registration/src/Validate.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Student on 6/27/2017.
- */
-public interface Validate
-{
-     boolean Validator(Address cím);
-}
--- temp/Termekek/src/menza/NincsIlyenTermékException.java
+++
@@ -1,8 +0,0 @@
-package menza;
-
-/**
- * Created by Student on 6/28/2017.
- */
-public class NincsIlyenTermékException extends Exception
-{
-}
