c4962a9b03371b2d24c71933cc98c60ba37f3d47
GaborVeress <veressgabi33@freemail.hu>
2017-07-04 11:00:49 +0200

    completed

---
+++ tesztmaven/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Gabor_Veress on 7/3/2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MEGY! YAY");
+    }
+}
---
+++ tesztmaven/target/run.bat
@@ -0,0 +1,2 @@
+java -jar teszt-maven-1.0.0-jar-with-dependencies.jar
+PAUSE
